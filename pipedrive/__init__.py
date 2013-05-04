685a4086690702daee7748eb1ffa3b698ad2e370
AJ Bahnken <aj@ajvb.me>
2013-05-03 20:47:49 -0700

    Replaced tabs with 4 spaces; re-wrote the http.request() calls. Removed unneeded stuff. Fixed PipedriveError. replaced urlencode with json.dumps for POST and PUT requests.

--- pipedrive/__init__.py
+++ pipedrive/__init__.py
@@ -8,53 +8,72 @@ from copy import copy
 PIPEDRIVE_API_URL = "https://api.pipedrive.com/v1/"
 
 class PipedriveError(Exception):
-	def __init__(self, response):
-		self.response = response
-	def __str__(self):
-		return self.response.get('error', 'No error provided')
+    def __init__(self, response):
+        self.response = response
+    def __str__(self):
+        return str(self.response)
 
 class IncorrectLoginError(PipedriveError):
-	pass
+    pass
 
 class Pipedrive(object):
-	
-	def _request(self, endpoint, data, method="POST"):
-		
-		if self.api_token:
-			data = copy(data)
-			data['api_token'] = self.api_token
-		
-		if method in ["POST", "PUT"]:
-			if 'id' not in data:
-				raise PipedriveError("No 'id' field, all updates require one.")
-			response, data = self.http.request("%s%s/%s?api_token=%s" % (PIPEDRIVE_API_URL, endpoint, data['id'], data['api_token']), method=method, body=urlencode(data), headers={'Content-Type': 'application/json'})
-		else:
-			response, data = self.http.request("%s%s?%s" % (PIPEDRIVE_API_URL, endpoint, urlencode(data)), method)
+    '''
+    Provides an interface for PipeDrive's API.
+    Official Documentation for PipeDrive's API: https://developers.pipedrive.com/v1
 
-		return json.loads(data)
+    When looking at the __getattr__() wrapper or _request():
+        endpoint = What you are making a request towards. e.g. "persons".
+        data = Dictionary of what to send.
+        method = HTTP Method to use. GET, POST, PUT, and DELETE are whats supported
 
-	def __init__(self, login = None, password = None):
-		self.http = Http()
-		if password:
-			response = self._request("/auth/login", {"login": login, "password": password})
+    GET: pipedrive.persons({'method': 'GET'})
+    POST: pipedrive.persons({'method': 'POST', 'name': 'YAY', 'email': [{'value': 'yay@pleasework.com'}]})
 
-			if 'error' in response:
-				raise IncorrectLoginError(response)
-			
-			self.api_token = response['authorization'][0]['api_token']
-		else:
-			# Assume that login is actually the api token
-			self.api_token = login
+    '''
 
-	def __getattr__(self, name):
-		def wrapper(data):
-			if 'method' in data: 
-				method = data['method'] 
-				del data['method']
-			else: 
-				method = "POST"
-			response = self._request(name, data, method)
-			if 'error' in response:
-				raise PipedriveError(response)
-			return response
-		return wrapper
+    def _request(self, endpoint, data, method="POST"):
+        if self.api_token:
+            data = copy(data)
+            qs = "%s%s?api_token=%s" % (PIPEDRIVE_API_URL, endpoint, self.api_token)
+        else:
+            #TODO throw error
+            pass
+
+        #In the below request, it assumes you entered the data 'correctly'
+        # e.g. pipdrive.persons({'method': 'POST', 'name':
+        if method in ["POST", "PUT"]:
+            response, data = self.http.request(qs,
+                                               method,
+                                               body=json.dumps(data),
+                                               headers={'Content-Type': 'application/json'}
+                                              )
+        else:
+            response, data = self.http.request(qs, method)
+
+        return json.loads(data)
+
+    def __init__(self, login = None, password = None):
+        self.http = Http()
+        if password:
+            response = self._request("/auth/login", {"login": login, "password": password})
+
+            if 'error' in response:
+                raise IncorrectLoginError(response)
+
+            self.api_token = response['authorization'][0]['api_token']
+        else:
+            # Assume that login is actually the api token
+            self.api_token = login
+
+    def __getattr__(self, endpoint):
+        def wrapper(data):
+            if 'method' in data:
+                method = data['method']
+                del data['method']
+            else:
+                method = "POST"
+            response = self._request(endpoint, data, method)
+            if 'error' in response:
+                raise PipedriveError(response)
+            return response
+        return wrapper

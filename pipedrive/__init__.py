d61dd5351b35f17285adf815cdaf4c0c779e5625
AJ Bahnken <aj@ajvb.me>
2013-05-03 21:42:09 -0700

    removed urlencode import; added TODO's; removed copy() usage; finished e.g. comment; added upper() to method so that 'post' will work.

--- pipedrive/__init__.py
+++ pipedrive/__init__.py
@@ -1,16 +1,18 @@
 from httplib2 import Http
-from urllib import urlencode
 import json
 from copy import copy
+from string import upper
 
 PIPEDRIVE_API_URL = "https://api.pipedrive.com/v1/"
 
+#TODO remove
 class PipedriveError(Exception):
     def __init__(self, response):
         self.response = response
     def __str__(self):
         return str(self.response)
 
+#TODO remove
 class IncorrectLoginError(PipedriveError):
     pass
 
@@ -32,7 +34,7 @@ class Pipedrive(object):
 
     def _request(self, endpoint, data, method="POST"):
         if self.api_token:
-            data = copy(data)
+            method = upper(method)
             if method in ["POST", "GET"]:
                 qs = "%s%s?api_token=%s" % (PIPEDRIVE_API_URL, endpoint, self.api_token)
             elif method in ["PUT", "DELETE"]:
@@ -43,7 +45,7 @@ class Pipedrive(object):
             pass
 
         #In the below request, it assumes you entered the data 'correctly'
-        # e.g. pipdrive.persons({'method': 'POST', 'name':
+        # e.g. pipdrive.persons({'method': 'POST', 'name': 'Roger'})
         if method in ["POST", "PUT"]:
             response, data = self.http.request(qs,
                                                method,
@@ -59,7 +61,7 @@ class Pipedrive(object):
         if password:
             response = self._request("/auth/login", {"login": login, "password": password})
 
-            if 'error' in response:
+            if 'error' in response: #TODO don't use custom Exceptions
                 raise IncorrectLoginError(response)
 
             self.api_token = response['authorization'][0]['api_token']
@@ -76,6 +78,6 @@ class Pipedrive(object):
                 method = "POST"
             response = self._request(endpoint, data, method)
             if 'error' in response:
-                raise PipedriveError(response)
+                raise PipedriveError(response) #TODO don't use custom Exceptions
             return response
         return wrapper
